4db8df1cdcf728fdbbd6b53b66c8dda7e6b51b6e
Ehtisham <ehtishamhaider.987@gmail.com>
2023-01-14 03:55:31 +0500

    cart almost done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use Illuminate\Support\Facades\DB;
 use Session;
 
 class ProductController extends Controller
@@ -44,5 +45,12 @@ class ProductController extends Controller
     {
         $userId=Session::get('user')['id'];
         return Cart::where('user_id',$userId)->count();
+    }
+    function cartList()
+    {
+        $userId=Session::get('user')['id'];
+        $products= DB::table('cart')->join('products','cart.product_id','=','products.id')->where('cart.user_id',$userId)->select('products.*')->get();
+
+        return view('cartlist',['products'=>$products]);
     }
 }
