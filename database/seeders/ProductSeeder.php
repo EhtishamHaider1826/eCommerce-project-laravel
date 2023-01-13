65bdbf4ded521d347b80231b3bf80da366b0a2a8
Ehtisham <ehtishamhaider.987@gmail.com>
2023-01-14 03:12:50 +0500

    logout and login done

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            [
+                'name'=>"Oppo mobile",
+                'price'=>"40,000",
+                "description"=>"A smartphone with 8 gb ram and much more features",
+                "category"=>"Oppo",
+                "gallery"=>"https://www.whatmobile.com.pk/admin/images/Oppo/OppoA164GB-b.jpg"
+
+            ],
+            [
+                'name'=>"Huwaeii y6s",
+                'price'=>"400,000",
+                "description"=>"A smartphone with 6 gb ram and much more features",
+                "category"=>"Huwaeii SmartPhones",
+                "gallery"=>"https://www.whatmobile.com.pk/admin/images/Huawei/HuaweiY6s-b.jpg"
+
+            ],
+            [
+                'name'=>"Huaweii s6",
+                'price'=>"220,000",
+                "description"=>"A smartphone with 3 gb ram and much more features",
+                "category"=>"Huwaeii SmartPhones",
+                "gallery"=>"https://www.whatmobile.com.pk/admin/images/Huawei/HuaweiY6s-b.jpg"
+
+            ],
+            [
+                'name'=>"LG mobile",
+                'price'=>"20,000",
+                "description"=>"A smartphone with 4 gb ram and much more features",
+                "category"=>"LG Smartphones",
+                "gallery"=>"https://www.whatmobile.com.pk/admin/images/LG/LGK51S-b.jpg"
+
+            ]
+        ]);
+    }
+    }
+
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -29,6 +29,39 @@
         height: 500px;
         padding-top: 100px;
     }
+
+    img.slider-img {
+        height: 400px !important
+    }
+
+    .custom-product {
+        height: 600px
+    }
+
+    .slider-text {
+        background-color: #93a79373 !important;
+    }
+
+    .trending-image {
+        height: 100px
+    }
+
+    .trending-item {
+        float: left;
+        width: 25%;
+    }
+
+    .trending-wrapper {
+        margin: 30px;
+    }
+
+    .detail-img {
+        height: 200px
+    }
+
+    .search-box {
+        width: 500px !important
+    }
 </style>
 
 </html>
